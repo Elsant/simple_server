ff2875809392f414a1e49842adddd4e20cae410d
Ivan Petrakov <i.petrakov@yandex.ru>
2022-07-06 13:24:35 +0200

    adds server constructor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let string = String::from("127.0.0.1:3000");
+    let server = Server::new(string);
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self { addr }
+    }
+
+    fn run(self) {
+        println!("Listening on {}", self.addr)
+    }
 }
