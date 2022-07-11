70d75beb655a97eef0f66fcc13691814a4cb714a
Ivan Petrakov <i.petrakov@yandex.ru>
2022-07-11 16:22:31 +0200

    extracts modules into files

---
+++ src/http/method.rs
@@ -0,0 +1,5 @@
+pub enum Method {
+    GET(String),
+    POST,
+    DELETE(u64)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,12 @@
-use http::request::Request;
+use http::Request;
+use http::Method;
 use server::Server;
+
+mod server;
+mod http;
 
 fn main() {
     let string = String::from("127.0.0.1:3000");
     let server = Server::new(string);
     server.run();
-}
-
-mod server {
-    pub struct Server {
-        addr: String,
-    }
-
-    impl Server {
-        pub fn new(addr: String) -> Self {
-            Self { addr }
-        }
-
-        pub fn run(self) {
-            println!("Listening on {}", self.addr)
-        }
-    }
-}
-
-mod http {
-    pub mod request {
-        use super::method::Method;
-
-        pub struct Request {
-            path: String,
-            query_string: Option<String>,
-            method: super::method::Method,
-        }
-    }
-
-    pub mod method {
-        pub enum Method {
-            GET(String),
-            POST,
-            DELETE(u64)
-        }
-    }
 }
