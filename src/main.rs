b5d7b9dbed8255e6f01556914800a1683bad7677
Ivan Petrakov <i.petrakov@yandex.ru>
2022-09-27 14:48:04 +0200

    adds handler for response

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 #![allow(dead_code)]
 
 use server::Server;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
+
 
 fn main() {
-    let string = String::from("127.0.0.1:3000");
+    let string = String::from("127.0.0.1:3100");
     let server = Server::new(string);
-    server.run();
+    server.run(WebsiteHandler);
 }
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,16 @@
-use crate::http::Request;
+use crate::http::{ParseError, Request, Response, StatusCode};
 use std::convert::TryFrom;
-use std::io::Read;
+use std::io::{Read};
 use std::net::TcpListener;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
 
 pub struct Server {
     addr: String,
@@ -12,7 +21,7 @@ impl Server {
         Self { addr }
     }
 
-    pub fn run(self) {
+    pub fn run(self, mut handler: impl Handler) {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
@@ -25,11 +34,13 @@ impl Server {
                         Ok(_) => {
                             println!("Received - {}", String::from_utf8_lossy(&mut buffer));
 
-                            match Request::try_from(&buffer as &[u8]) {
-                                Ok(request) => {
-                                    dbg!(request);
-                                },
-                                Err(err) => println!("{:?}", err)
+                            let response =  match Request::try_from(&buffer as &[u8]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
                             }
                         },
                         Err(err) => println!("{:?}", err),
